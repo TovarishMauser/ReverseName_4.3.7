e294e7509ef20896438d1ae5e99fe28db29f7152
Tovarish Mauser <email@example.ru>
2021-08-24 20:19:22 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name;
+
+            name = Console.ReadLine();
+
+            int j = name.Length;
+            int i = 0;
+
+            char[] result = new char[name.Length + 1];
+
+
+            while ((j > 0) & (i < name.Length + 1))
+            {
+                
+                result[j] = name[i];
+                i++;
+                j--;
+            }
+
+            for (int k = 0; k < name.Length + 1; k++)
+            {
+                Console.Write(result[k]);
+            }
+            
+        }
+    }
+}
